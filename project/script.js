4ee45671757cae415f2e1d22e6dcecf129f313b0
Ghostachio <mansour.hamza2.0@gmail.com>
2022-12-04 17:39:12 +0100

    adding files

--- project/script.js
+++ project/script.js
@@ -1,8 +1,22 @@
 
-    let score = 0
+//    Different variable of the game
+   
+   let playerScore = 0;
+    let computerScore = 0;
     let rock = 'rock';
     let paper = 'paper';
     let scisors = 'scisors';
+
+    // game buttons
+
+
+
+
+
+
+
+
+    // game logic
 
 
 function getComputerChoice(){
@@ -24,23 +38,46 @@ function roundPlay(playerPlay, getComputerChoice){
         if (playerPlay === getComputerChoice){
             return tie
         } else if ((playerPlay === rock) && (getComputerChoice === paper)){
+            computerScore++
+            console.log(computerScore)
             return lose;
         } else if (( playerPlay === paper) && (getComputerChoice === scisors)){
+            computerScore++
+            console.log(computerScore)
             return lose;
         }else if (( playerPlay === scisors) && (getComputerChoice === rock)){
+            computerScore++
+            console.log(computerScore)
             return lose;
         }else{
+            playerScore++
+            console.log(playerScore)
             return win;
         }
     }
 
     
     
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; playerScore < 5 && computerScore < 5; i++) {
         let playerPlay = prompt(('enter your move here').toLowerCase());
 
-        console.log(roundPlay(playerPlay, getComputerChoice()))  
+        console.log(roundPlay(playerPlay, getComputerChoice()))
+        endPlay()  
      }
+       
+     
+     
+     // logic to play only five rounds
+
+    //  function endPlay(){
+    //     if (playerScore == 5){
+    //         console.log("kabatiha")
+    //     } else if (computerScore == 5)  {
+    //         console.log('noud 3la slamtek')
+    //     }
+    //  }
+
+
 
      
 
